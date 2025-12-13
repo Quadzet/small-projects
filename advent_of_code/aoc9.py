e0383c558771eeba00f4654a4c81d27c0ab1d0b3
JoakimWellmar <joakim.wellmar@pm.me>
2025-12-13 11:02:25 +0100

    Fix logical errors in aoc9p2 list comprehension solution. Still too slow though..

--- advent_of_code/aoc9.py
+++ advent_of_code/aoc9.py
@@ -5,15 +5,8 @@ log_interval = 1
 call = 0
 
 def is_valid(p1, p2, red_tiles, connections):
-    global call, log_interval
-    call += 1
-    # print(f"p1: {p1}, p2: {p2}")
     x1, y1 = p1
     x2, y2 = p2
-
-    if call >= log_interval:
-        print(f"Calc number {call}.")
-        log_interval += log_interval//10
 
     left = min(x1, x2)
     right = max(x1, x2)
@@ -32,43 +25,33 @@ def is_valid(p1, p2, red_tiles, connections):
     # Can be combined/optimized ofc
     # (9, 5), (2, 3)
 
-    """
-    for x in range(left + 1, right):
-        lines_enc = 0
-        if (x, bot) in red_tiles:
-            continue
-        for y in range(0, bot+1):
-            for t1, t2 in connections:
-                if min(t1[0], t2[0]) <= x <= max(t1[0], t2[0]) and t1[1] == y == t2[1]:
-                    lines_enc += 1
-    """
+    red_tiles_set = set(red_tiles)
 
-    # TODO handle vertical lines when looking horizantally etc.........
+    lines = [(t1, t2) for t1, t2 in connections if t1[1] <= bot and t2[1] <= bot and not ((t1[0] > right and t2[0] > right) or (t1[0] < left and t2[0] < left))]
 
-    lines = [(t1, t2) for t1, t2 in connections if t1[1] == t2[1] <= bot and not ((t1[0] > right and t2[0] > right) or (t1[0] < left and t2[0] < left))]
+    x_to_check = [x for x in range(left+1, right) if (x, bot) not in red_tiles_set]
+    lines_enc = len([1 for t1, t2 in lines for x in x_to_check if min(t1[0], t2[0]) <= x <= max(t1[0], t2[0])])
 
-    for x in range(left+1, right):
-        if (x, bot) in red_tiles:
-            continue
-        lines_enc = len([1 for t1, t2 in lines if min(t1[0], t2[0]) <= x <= max(t1[0], t2[0])])
-
-        if lines_enc % 2 != 1:
-            return False
-    if len([(t1, t2) for t1, t2 in connections if left < t1[1] == t2[1] < right and not ((t1[0] > right and t2[0] > right) or (t1[0] < left and t2[0] < left))]) > 0:
+    if lines_enc % 2 != 1:
+        return False
+    if len([(t1, t2) for t1, t2 in connections if bot < t1[1] == t2[1] < top and not ((t1[0] >= right and t2[0] >= right) or (t1[0] <= left and t2[0] <= left))]) > 0:
         return False
 
 
-    lines = [(t1, t2) for t1, t2 in connections if t1[0] == t2[0] <= left and not ((t1[1] > top and t2[1] > top) or (t1[1] < bot and t2[1] < bot))]
+    lines = [(t1, t2) for t1, t2 in connections if t1[0] <= left and t2[0] <= left and not ((t1[1] > top and t2[1] > top) or (t1[1] < bot and t2[1] < bot))]
 
-    for y in range(bot+1, top):
-        lines_enc = len([1 for t1, t2 in lines 
-            if (left, y) not in red_tiles and min(t1[1], t2[1]) <= y <= max(t1[1], t2[1])])
+    y_to_check = [y for y in range(bot+1, top) if (left, y) not in red_tiles_set]
+    lines_enc = len([1 for t1, t2 in lines for y in y_to_check if min(t1[1], t2[1]) <= y <= max(t1[1], t2[1])])
 
-        if lines_enc % 2 != 1:
-            return False
-    if len([(t1, t2) for t1, t2 in connections if bot < t1[1] == t2[1] < top and not ((t1[0] > right and t2[0] > right) or (t1[0] < left and t2[0] < left))]) > 0:
+    if lines_enc % 2 != 1:
         return False
 
+    matches = [(t1, t2) for t1, t2 in connections if left < t1[0] == t2[0] < right and not ((t1[1] >= top and t2[1] >= top) or (t1[1] <= bot and t2[1] <= bot))]
+    if len(matches) > 0:
+        print(matches)
+        return False
+
+    print("OK")
     return True
 
 def aoc9p2():
@@ -88,9 +71,20 @@ def aoc9p2():
     rt_matrix = [[(a, b, (abs(a[0] - b[0])+1)*(abs(a[1] - b[1])+1)) for a in red_tiles] for b in red_tiles]
     # is_valid((9, 5), (2, 3), red_tiles, connections)
     # return
-    max_area = max([i[2] for line in rt_matrix for i in line if is_valid(i[0], i[1], red_tiles, connections)])
+    rt_matrix = sorted([item for line in rt_matrix for item in line], key=lambda x: x[2])
 
-    print(f"Max: {max_area}")
+    i = 1
+    while rt_matrix:
+        rect = rt_matrix.pop()
+        print(f"#{i}\tChecking rect {rect[0]}, {rect[1]}. Area: {rect[2]}.")
+        if is_valid(rect[0], rect[1], red_tiles, connections):
+            print(f"Max: {rect[2]}")
+            return
+        i += 1
+    print("No valid rectangels found.")
+    #max_area = max([i[2] for line in rt_matrix for i in line if is_valid(i[0], i[1], red_tiles, connections)])
+
+    # print(f"Max: {max_area}")
 
 def aoc9p1():
     red_tiles = [ ]
