ee9cf307ba058d1cf1dc98928e6cbdfda4f51352
JoakimWellmar <joakim.wellmar@pm.me>
2025-12-13 19:45:57 +0100

    Complete aoc11p2.

--- advent_of_code/aoc11.py
+++ advent_of_code/aoc11.py
@@ -13,38 +13,32 @@ def output(current_device: str, current_path: list, paths: dict):
             output(dest, current_path, paths)
 
 @lru_cache
-def output2(current_device: str, current_path: str, has_fft: bool, has_dac):
+def output2(current_device: str, end_device: str):
+    if current_device == end_device:
+        return 1
+    elif current_device == "out":
+        return 0
+
     res = 0
     global PATHS
     for dest in PATHS[current_device]:
-        if dest == "fft":
-            res += output2(dest, True, has_dac)
-        if dest == "dac":
-            if has_fft:
-                res += output2(dest, has_fft, True)
-            else:
-                pass
-        elif dest == "out":
-            if has_fft and has_dac:
-                res += 1
-        else:
-            res += output2(dest, has_fft, has_dac)
+        res += output2(dest, end_device)
     return res
-
 
 def aoc11p2():
     global PATHS
     with open("aoc11_data", "r") as f:
         while line := f.readline().strip():
             src = re.search('^([^:]*):', line).group(1)
-            print(f"src: {src}")
             dests = re.search(':(.*)', line).group(1).strip().split(' ')
-            print(f"dests: {dests}")
-
             PATHS[src] = dests
-    # print(PATHS)
-    res = output2("svr", False, False)
-    print(f"Number of paths: {res}")
+    res1 = output2("svr", "fft")
+    print(f"From svr to fft: {res1}")
+    res2 = output2("fft", "dac")
+    print(f"From fft to dac: {res2}")
+    res3 = output2("dac", "out")
+    print(f"From dac to out: {res3}")
+    print(f"Total: {res1 * res2 * res3}.")
 
 def aoc11p1():
     sum = 0
